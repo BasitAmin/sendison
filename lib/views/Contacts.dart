134d69a122b0ea4d05d84b2f93678291972ce41b
MobeenArshad <mobeenarshad03@gmail.com>
2021-03-31 19:38:56 +0500

    fourth screen fixed

--- lib/views/Contacts.dart
+++ lib/views/Contacts.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 import 'package:sendison/views/BottomNavBar.dart';
 import 'package:sendison/views/Calls.dart';
@@ -14,51 +15,68 @@ class Contacts extends StatefulWidget {
 class _ContactsState extends State<Contacts> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Container(
-        width: Get.width,
-        height: Get.height,
-        padding: EdgeInsets.all(10),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              child: Text('Contacts',style: TextStyle(
-                  fontSize: 50,fontWeight: FontWeight.w600),),
-            ),
-            SizedBox(height: 30,),
-            Container(
-              child: Icon(Icons.group_rounded,size: 150,),
-            ),
-            Container(
-              child: Text('Import Your \n Existing Contacts',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    fontSize: 25,
-                    fontWeight: FontWeight.w600),),
-            ),
-            SizedBox(height: 70,),
-            MaterialButton(
-              minWidth: Get.width/1.8,
-              onPressed: (){
-                Get.to(BottomNavBar());
-              },
-              color: Color(0xff0FBE6C),
-              height: 45,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(6) ),
-              child: Text('Import',style: TextStyle(
-                  fontSize: 20,fontWeight: FontWeight.w600,color: Colors.white),),
-            ),
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Container(
+          width: Get.width,
+          height: Get.height,
+          padding: EdgeInsets.only(
+              top: 60,
+              left: 20,
+              right: 20,
+              bottom: Get.width/4),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                height: Get.width/4,
+                child: Text('Contacts',style: TextStyle(
+                    fontSize: 50,fontWeight: FontWeight.w600),),
+              ),
 
-            FlatButton(
-                onPressed: (){},
-                child: Text('Later',style: TextStyle(
-                    fontSize: 20, fontWeight: FontWeight.bold,
-                    color: Colors.grey.withOpacity(0.5)),)
-            ),
-          ],
+              Container(
+                width: Get.width,
+                height: Get.width/3,
+                child: SvgPicture.asset('assets/images/LoginSignUpScreens/Import Contacts Icon.svg',
+              ),
+              ),
+
+              Container(
+                height: Get.width/3,
+                child: Text('Import Your \n Existing Contacts',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.w600),),
+              ),
+
+              Container(
+                padding: EdgeInsets.only(left: 40,right: 40),
+                child: MaterialButton(
+                  minWidth: Get.width,
+                  onPressed: (){
+                    Get.to(BottomNavBar());
+                  },
+                  color: Color(0xff0FBE6C),
+                  height: 45,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(6) ),
+                  child: Text('Import',style: TextStyle(
+                      fontSize: 22,fontWeight: FontWeight.w600,color: Colors.white),),
+                ),
+              ),
+
+              Container(
+                child: FlatButton(
+                    onPressed: (){},
+                    child: Text('Later',style: TextStyle(
+                        fontSize: 22, fontWeight: FontWeight.bold,
+                        color: Colors.grey.withOpacity(0.5)),)
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
